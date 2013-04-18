9be04eee8ea449b38c163bbc5dcf6e1933122bd6
Yuriy Vasiyarov <yvasiyarov@gmail.com>
2013-04-19 00:56:40 +0400

    Get redirect url packet is sent

--- protocol.go
+++ protocol.go
@@ -1,16 +1,14 @@
 package gorelic
 
 import (
-//	"encoding/json"
+	"encoding/json"
+	"errors"
 	"fmt"
-    "net/http"
-    "net/url"
-    "strings"
-    "io/ioutil"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
 )
-
-//redirect_host = send_request(None, url, 'get_redirect_host',
-//                license_key)
 
 var LicenseKey string
 var CollectorUrl string
@@ -19,24 +17,27 @@ const AGENT_VERSION = "0.1"
 const START_COLLECTOR_URL = "http://collector.newrelic.com"
 const COLLECTOR_METHOD = "/agent_listener/invoke_raw_method"
 
+type JsonParams map[string]interface{}
 type Packet struct {
-	params  url.Values
-	header  http.Header
-	url     string
-    responseBody []byte
+	params       url.Values
+	header       http.Header
+	url          string
+	rawResponse  []byte
+	responseCode int
+	jsonParams   JsonParams
 }
 
 func NewPacket() *Packet {
 	p := &Packet{
-		params:  url.Values{},
-		header:  http.Header{},
+		params:     url.Values{},
+		header:     http.Header{},
+		jsonParams: JsonParams{},
 	}
 	p.params.Add("license_key", LicenseKey)
 	p.params.Add("protocol_version", "12")
 	p.params.Add("marshal_format", "json")
 
-	p.header["User-Agent"] = []string{"NewRelic-PythonAgent/1.10.2.38 (Python 2.7.3 linux2)"}
-	//p.header["User-Agent"] = []string{fmt.Sprintf("NewRelic-GoAgent/%s", AGENT_VERSION)}
+	p.header["User-Agent"] = []string{fmt.Sprintf("NewRelic-GoAgent/%s", AGENT_VERSION)}
 	p.header["Accept-Encoding"] = []string{"identity, deflate, compress, gzip"}
 	p.header["Accept"] = []string{"*/*"}
 	p.header["Content-Encoding"] = []string{"identity"}
@@ -50,37 +51,46 @@ func NewPacketGetRedirectHost() *Packet {
 	packet.params.Add("method", "get_redirect_host")
 	packet.url = START_COLLECTOR_URL
 
-    return packet
+	return packet
 }
 
 func (packet *Packet) Send() error {
-
-    bodyContent := packet.params.Encode()
-    //if len(bodyContent) > 64*1024 {
+	bodyContent, err := json.Marshal(packet.jsonParams)
+	if err != nil {
+		return err
+	}
+	//if len(bodyContent) > 64*1024 {
 	//    packet.header["Content-Encoding"] = []string{"deflate"}
-    //}
+	//}
 
-	body := ioutil.NopCloser(strings.NewReader(""))
-	//body := ioutil.NopCloser(strings.NewReader(bodyContent))
-    //fmt.Println(packet.url + COLLECTOR_METHOD + "?" + bodyContent)
+	body := ioutil.NopCloser(strings.NewReader(string(bodyContent)))
 
-    if req, err := http.NewRequest("POST", packet.url + COLLECTOR_METHOD + "?" + bodyContent, body); err != nil {
-        return err
-    } else {
-	    //packet.header["Content-Encoding"] = []string{"identity"}
-        req.Header = packet.header
-        req.ContentLength = 0
-        fmt.Printf("req:  %s", req.URL.String()) 
-        if resp, err := http.DefaultClient.Do(req); err != nil {
-            return err
-        } else {
-	        defer resp.Body.Close()
-	        if b, err := ioutil.ReadAll(resp.Body); err != nil {
-                return err
-            } else {
-                packet.responseBody = b
-            }
-        }
-    }
-    return nil
+	urlParams := packet.params.Encode()
+	if req, err := http.NewRequest("POST", packet.url+COLLECTOR_METHOD+"?"+urlParams, body); err != nil {
+		return err
+	} else {
+		req.Header = packet.header
+		req.ContentLength = int64(len(bodyContent))
+		if resp, err := http.DefaultClient.Do(req); err != nil {
+			return err
+		} else {
+			defer resp.Body.Close()
+			if responseBody, err := ioutil.ReadAll(resp.Body); err != nil {
+				return err
+			} else {
+				packet.responseCode = resp.StatusCode
+				packet.rawResponse = responseBody
+			}
+		}
+	}
+	return nil
+}
+
+func (packet *Packet) GetResponse() (interface{}, error) {
+	if packet.responseCode != 200 {
+		return nil, errors.New(string(packet.rawResponse))
+	}
+	var result interface{}
+	err := json.Unmarshal(packet.rawResponse, &result)
+	return result, err
 }
--- protocol_test.go
+++ protocol_test.go
@@ -1,17 +1,16 @@
 package gorelic
 
 import (
-    "testing"
-    "fmt"
+	"fmt"
+	"testing"
 )
 
-
 func TestGetRedirectHost(t *testing.T) {
-    packet := NewPacketGetRedirectHost()
-    if err := packet.Send(); err != nil {
-        t.Errorf("Can not send get redirect host packet: %#v \n", err)
-    } else {
-        fmt.Printf("Request sent: %s \n", string(packet.responseBody))
-    } 
+	packet := NewPacketGetRedirectHost()
+	if err := packet.Send(); err != nil {
+		t.Errorf("Can not send get redirect host packet: %#v \n", err)
+	} else {
+		resp, err := packet.GetResponse()
+		fmt.Printf("Got response: %#v \nerror: %v\n", resp, err)
+	}
 }
-

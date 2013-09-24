5b3077d4f15dd3a90ed6e92d7c321d91c97425eb
Yuriy Vasiyarov <yvasiyarov@gmail.com>
2013-09-24 08:14:05 +0700

    First GC metrics finally works

--- gc_metrics.go
+++ gc_metrics.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"fmt"
 	metrics "github.com/yvasiyarov/go-metrics"
-	"runtime/debug"
-    "time"
-    "github.com/yvasiyarov/newrelic_platform_go"
+	"github.com/yvasiyarov/newrelic_platform_go"
+	"time"
 )
 
 //metrics:
@@ -12,21 +12,22 @@ import (
 //Total durations of all GC calls
 //Quantiles of GC call durations 
 
-type GCMetricaDataSource metrics.Registry 
+type GCMetricaDataSource metrics.Registry
 
 func NewGCMetricaDataSource() GCMetricaDataSource {
-    r := metrics.NewRegistry()
+	r := metrics.NewRegistry()
 
-    metrics.RegisterDebugGCStats(r)
-    go metrics.CaptureDebugGCStats(r, time.Duration(GC_STAT_CAPTURING_INTERVAL_IN_SECONDS) * time.Second)
-    return r;
+	metrics.RegisterDebugGCStats(r)
+	go metrics.CaptureDebugGCStats(r, time.Duration(GC_STAT_CAPTURING_INTERVAL_IN_SECONDS)*time.Second)
+	return r
 }
 
-type GCMetrica struct{
-    dataSource GCMetricaDataSource
-    basePath string 
-    name string 
-    units string
+type GCMetrica struct {
+	dataSource    GCMetricaDataSource
+	basePath      string
+	name          string
+	units         string
+	dataSourceKey string
 }
 
 func (metrica *GCMetrica) GetName() string {
@@ -36,25 +37,31 @@ func (metrica *GCMetrica) GetUnits() string {
 	return metrica.units
 }
 func (metrica *GCMetrica) GetValue() (float64, error) {
-    if val := metrica.dataSource.Get(metrica.name); val == nil {
-        return 0, fmt.Errorf("Metrica with name %s is not registered\n")
-    } else {
-        return float64(val), nil
-    }
+	if valueContainer := metrica.dataSource.Get(metrica.dataSourceKey); valueContainer == nil {
+		return 0, fmt.Errorf("Metrica with name %s is not registered\n", metrica.name)
+	} else {
+		switch valueContainer := valueContainer.(type) {
+		default:
+			return 0, fmt.Errorf("Metrica container has unexpected type: %T\n", valueContainer)
+		case *metrics.StandardGauge:
+			return float64(valueContainer.Value()), nil
+		}
+	}
 }
 
 func addGCMericsToComponent(component newrelic_platform_go.IComponent) {
-    metrics := []*GCMetrica{
-        &GCMetrica{
-            name: "NumberOfGCCalls",
-            units: "calls"
-        }
-    }
+	metrics := []*GCMetrica{
+		&GCMetrica{
+			name:          "NumberOfGCCalls",
+			units:         "calls",
+			dataSourceKey: "debug.GCStats.NumGC",
+		},
+	}
 
-    ds := NewGCMetricaDataSource();
-    for _, m := range metrics {
-        m.basePath = "Runtime/GC/"
-        m.dataSource = ds
-        component.AddMetrica(m)
-    }
+	ds := NewGCMetricaDataSource()
+	for _, m := range metrics {
+		m.basePath = "Runtime/GC/"
+		m.dataSource = ds
+		component.AddMetrica(m)
+	}
 }

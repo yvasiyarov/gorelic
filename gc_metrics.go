84714c67ba562871243610b28b9a962cec9dbd3e
Vasiyarov Yuriy <varyous@gmail.com>
2013-10-04 16:09:25 +0700

    Refactor all code. Move go metrics wrapper in one place

--- gc_metrics.go
+++ gc_metrics.go
@@ -1,121 +1,31 @@
 package main
 
 import (
-	"fmt"
 	metrics "github.com/yvasiyarov/go-metrics"
 	"github.com/yvasiyarov/newrelic_platform_go"
 	"time"
 )
 
-//metrics:
-//Total durations of all GC calls
-//Quantiles of GC call durations 
-
-type GCMetricaDataSource struct {
-	*metrics.StandardRegistry
-}
-
-func NewGCMetricaDataSource() GCMetricaDataSource {
+func NewGCMetricaDataSource() GoMetricaDataSource {
 	r := metrics.NewRegistry()
 
 	metrics.RegisterDebugGCStats(r)
 	go metrics.CaptureDebugGCStats(r, time.Duration(GC_POLL_INTERVAL_IN_SECONDS)*time.Second)
-	return GCMetricaDataSource{r}
+	return GoMetricaDataSource{r}
 }
 
-func (ds GCMetricaDataSource) GetValue(key string, statFunction string) (float64, error) {
-	if valueContainer := ds.Get(key); valueContainer == nil {
-		return 0, fmt.Errorf("Metrica with name %s is not registered\n", key)
-	} else {
-		switch valueContainer := valueContainer.(type) {
-		default:
-			return 0, fmt.Errorf("Metrica container has unexpected type: %T\n", valueContainer)
-		case *metrics.StandardGauge:
-			return float64(valueContainer.Value()), nil
-		case *metrics.StandardHistogram:
-			switch statFunction {
-			default:
-				return 0, fmt.Errorf("Unsupported stat function for histogram: %s\n", statFunction)
-			case "Max":
-				return float64(valueContainer.Max()), nil
-			case "Min":
-				return float64(valueContainer.Min()), nil
-			case "Mean":
-				return float64(valueContainer.Mean()), nil
-			case "StdDev":
-				return float64(valueContainer.StdDev()), nil
-			case "Percentile95":
-				return float64(valueContainer.Percentile(0.95)), nil
-			}
-		}
-	}
-}
-
-type GCMetrica struct {
-	dataSource    GCMetricaDataSource
-	basePath      string
-	name          string
-	units         string
-	dataSourceKey string
-	previousValue float64
-	statFunction  string
-}
-
-func (metrica *GCMetrica) GetName() string {
-	name := metrica.basePath + metrica.name
-	if metrica.statFunction != "" {
-		name += "/" + metrica.statFunction
-	}
-	return name
-}
-func (metrica *GCMetrica) GetUnits() string {
-	return metrica.units
-}
-func (metrica *GCMetrica) GetValue() (float64, error) {
-	if currentValue, err := metrica.dataSource.GetValue(metrica.dataSourceKey, metrica.statFunction); err != nil {
-		return 0, err
-	} else {
-		value := currentValue - metrica.previousValue
-		metrica.previousValue = currentValue
-		return value, nil
-	}
-}
 
 func addGCMericsToComponent(component newrelic_platform_go.IComponent) {
-	metrics := []*GCMetrica{
-		&GCMetrica{
+	metrics := []*BaseGoMetrica{
+		&BaseGoMetrica{
 			name:          "NumberOfGCCalls",
 			units:         "calls",
 			dataSourceKey: "debug.GCStats.NumGC",
 		},
-		&GCMetrica{
+		&BaseGoMetrica{
 			name:          "PauseTotalTime",
 			units:         "nanoseconds",
 			dataSourceKey: "debug.GCStats.PauseTotal",
-		},
-		&GCMetrica{
-			name:          "GCTime",
-			units:         "nanoseconds",
-			dataSourceKey: "debug.GCStats.Pause",
-			statFunction:  "Max",
-		},
-		&GCMetrica{
-			name:          "GCTime",
-			units:         "nanoseconds",
-			dataSourceKey: "debug.GCStats.Pause",
-			statFunction:  "Min",
-		},
-		&GCMetrica{
-			name:          "GCTime",
-			units:         "nanoseconds",
-			dataSourceKey: "debug.GCStats.Pause",
-			statFunction:  "Mean",
-		},
-		&GCMetrica{
-			name:          "GCTime",
-			units:         "nanoseconds",
-			dataSourceKey: "debug.GCStats.Pause",
-			statFunction:  "Percentile95",
 		},
 	}
 
@@ -123,6 +33,34 @@ func addGCMericsToComponent(component newrelic_platform_go.IComponent) {
 	for _, m := range metrics {
 		m.basePath = "Runtime/GC/"
 		m.dataSource = ds
+		component.AddMetrica(&GaugeMetrica{m})
+	}
+
+	histogramMetrics := []*HistogramMetrica{
+		&HistogramMetrica{
+			statFunction:  HISTOGRAM_MAX,
+            BaseGoMetrica: &BaseGoMetrica{name: "Max"},
+		},
+		&HistogramMetrica{
+			statFunction:  HISTOGRAM_MIN,
+            BaseGoMetrica: &BaseGoMetrica{name: "Min"},
+		},
+		&HistogramMetrica{
+			statFunction:  HISTOGRAM_MEAN,
+            BaseGoMetrica: &BaseGoMetrica{name: "Mean"},
+		},
+		&HistogramMetrica{
+			statFunction:  HISTOGRAM_PERCENTILE,
+            percentileValue: 0.95,
+            BaseGoMetrica: &BaseGoMetrica{name: "Percentile95"},
+		},
+	}
+	for _, m := range histogramMetrics {
+        m.BaseGoMetrica.units = "nanoseconds"
+        m.BaseGoMetrica.dataSourceKey = "debug.GCStats.Pause"
+        m.BaseGoMetrica.basePath = "Runtime/GC/GCTime/"
+        m.BaseGoMetrica.dataSource =  ds
+
 		component.AddMetrica(m)
 	}
 }
---
+++ gometrica.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"fmt"
+	metrics "github.com/yvasiyarov/go-metrics"
+)
+
+const (
+    HISTOGRAM_MIN = iota
+    HISTOGRAM_MAX
+    HISTOGRAM_MEAN
+    HISTOGRAM_PERCENTILE
+    HISTOGRAM_STD_DEV
+    HISTOGRAM_VARIANCE
+    NO_HISTOGRAM_FUNCTIONS
+)
+
+type GoMetricaDataSource struct {
+	*metrics.StandardRegistry
+}
+
+func (ds GoMetricaDataSource) GetGaugeValue(key string) (float64, error) {
+	if valueContainer := ds.Get(key); valueContainer == nil {
+		return 0, fmt.Errorf("Metrica with name %s is not registered\n", key)
+    } else if gauge, ok := valueContainer.(metrics.Gauge); ok {
+        return float64(gauge.Value()), nil
+    } else {
+        return 0, fmt.Errorf("Metrica container has unexpected type: %T\n", valueContainer)
+    }
+}
+
+func (ds GoMetricaDataSource) GetHistogramValue(key string, statFunction int, percentile float64) (float64, error) {
+	if valueContainer := ds.Get(key); valueContainer == nil {
+		return 0, fmt.Errorf("Metrica with name %s is not registered\n", key)
+    } else if histogram, ok := valueContainer.(metrics.Histogram); ok {
+        switch statFunction {
+        default:
+            return 0, fmt.Errorf("Unsupported stat function for histogram: %s\n", statFunction)
+        case HISTOGRAM_MAX:
+            return float64(histogram.Max()), nil
+        case HISTOGRAM_MIN:
+            return float64(histogram.Min()), nil
+        case HISTOGRAM_MEAN:
+            return float64(histogram.Mean()), nil
+        case HISTOGRAM_STD_DEV:
+            return float64(histogram.StdDev()), nil
+        case HISTOGRAM_VARIANCE:
+            return float64(histogram.Variance()), nil
+        case HISTOGRAM_PERCENTILE:
+            return float64(histogram.Percentile(percentile)), nil
+        }
+	} else {
+        return 0, fmt.Errorf("Metrica container has unexpected type: %T\n", valueContainer)
+	}
+}
+
+type BaseGoMetrica struct {
+	dataSource    GoMetricaDataSource
+	basePath      string
+	name          string
+	units         string
+	dataSourceKey string
+}
+func (metrica *BaseGoMetrica) GetName() string {
+	return metrica.basePath + metrica.name
+}
+
+func (metrica *BaseGoMetrica) GetUnits() string {
+	return metrica.units
+}
+
+
+type GaugeMetrica struct {
+    *BaseGoMetrica
+}
+func (metrica *GaugeMetrica) GetValue() (float64, error) {
+	return metrica.dataSource.GetGaugeValue(metrica.dataSourceKey)
+}
+
+type GaugeIncMetrica struct {
+	*BaseGoMetrica
+	previousValue float64
+}
+func (metrica *GaugeIncMetrica) GetValue() (float64, error) {
+	if currentValue, err := metrica.dataSource.GetGaugeValue(metrica.dataSourceKey); err != nil {
+		return 0, err
+	} else {
+		value := currentValue - metrica.previousValue
+		metrica.previousValue = currentValue
+		return value, nil
+	}
+}
+
+
+type HistogramMetrica struct {
+	*BaseGoMetrica
+    statFunction int
+    percentileValue float64
+}
+
+func (metrica *HistogramMetrica) GetValue() (float64, error) {
+	return metrica.dataSource.GetHistogramValue(metrica.dataSourceKey, metrica.statFunction, metrica.percentileValue)
+}
+
